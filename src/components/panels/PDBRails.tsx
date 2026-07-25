e274a1d0dbeb4f4f764b278988228556dd53cbaa
Nathaniel <va7nfh@gmail.com>
2026-07-25 13:17:37 -0400

    Fixes for new pdb (#46)
    * fix: new pdb
    
    * node names
    
    ---------
    
    Co-authored-by: Software Lead (Base Station) <softlead@curover.ca>

--- src/components/panels/PDBRails.tsx
+++ src/components/panels/PDBRails.tsx
@@ -6,12 +6,12 @@ import { useROS } from '@/ros/ROSContext';
 
 const LABELS = [
   "12V #1",
-  "12V #2",
   "12V #3",
+  "12V #2",
   "12V #4",
   "5V #1",
-  "5V #2",
   "5V #3",
+  "5V #2",
 ];
 
 const PDBRailsPanel: React.FC = () => {
